b66f4e615b80311fc5dc160f79f39ada4a1c4d53
devesh-y <ydevesh136@gmail.com>
2024-03-06 12:30:47 +0530

    devtool =false

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,11 @@ const path = require(`path`);
 
 module.exports = {
   webpack: {
+    configure: (webpackConfig) => {
+      // Disable source map generation
+      webpackConfig.devtool = false;
+      return webpackConfig;
+    },
     alias: {
       "@": path.resolve(__dirname, "src/"),
     },
